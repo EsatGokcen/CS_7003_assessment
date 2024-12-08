df178ff9e09073cc9fe5e54641619973ebf49cda
Esat Gokcen <esatgokcen173@gmail.com>
2024-12-08 22:40:00 +0000

    created a session and added data to employee table

--- src/data_access_layer/Users/employee_table.py
+++ src/data_access_layer/Users/employee_table.py
@@ -1,8 +1,10 @@
 from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import declarative_base, sessionmaker
 
 engine = create_engine('sqlite:///employee.db')
 Base = declarative_base()
+
+# Create table
 
 class Employee(Base):
 
@@ -13,3 +15,18 @@ class Employee(Base):
     email = Column(String, unique=True, nullable=False)
     password = Column(String, nullable=False)
     salary = Column(Integer)
+
+Base.metadata.create_all(engine)
+
+# Create session and add data to table
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+employee1 = Employee(name='Esat Gokcen', email='esatgokcen@gmail.com', password='KingSnake31!', salary=40000)
+employee2 = Employee(name='Taha Tariq', email='taha@hotmail.com', password='TahaisBest!', salary=30000)
+
+session.add_all([employee1, employee2])
+session.commit()
+
+session.close()
