cf37e208c3d3ae9e62d930271813932cfde59f27
Esat Gokcen <esatgokcen173@gmail.com>
2024-12-08 22:43:31 +0000

    created a session and added data to employee roles table

--- src/data_access_layer/Users/employee_roles.py
+++ src/data_access_layer/Users/employee_roles.py
@@ -1,8 +1,10 @@
 from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import declarative_base, sessionmaker
 
 engine = create_engine('sqlite:///employee_roles.db')
 Base = declarative_base()
+
+# Create table
 
 class Roles(Base):
 
@@ -10,3 +12,18 @@ class Roles(Base):
 
     role_id = Column(Integer, primary_key=True)
     role_name = Column(String)
+
+Base.metadata.create_all(engine)
+
+# Create session and add data to table
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+role1 = Roles(role_name='Manager')
+role2 = Roles(role_name='Barista')
+
+session.add_all([role1, role2])
+session.commit()
+
+session.close()
