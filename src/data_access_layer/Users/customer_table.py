ee8c70eda35864c0ce438d3c1da07682e96b7828
Esat Gokcen <esatgokcen173@gmail.com>
2024-12-08 22:32:42 +0000

    created a session and added data to customers table

--- src/data_access_layer/Users/customer_table.py
+++ src/data_access_layer/Users/customer_table.py
@@ -1,8 +1,10 @@
 from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import declarative_base, sessionmaker
 
 engine = create_engine('sqlite:///customer.db')
 Base = declarative_base()
+
+# Create Table
 
 class Customer(Base):
 
@@ -12,4 +14,19 @@ class Customer(Base):
     name = Column(String, nullable=False)
     email = Column(String, unique=True)
     password = Column(String, nullable=False)
+
+Base.metadata.create_all(engine)
+
+# Create session and add data to table
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+customer1 = Customer(name='Esra Gokcen', email='esrag@mail.com', password='gok123')
+customer2 = Customer(name='Eda Gokcen', email='edagokcen@hotmail.com', password='Pirtik24!')
+
+session.add_all([])
+session.commit()
+
+session.close()
 
