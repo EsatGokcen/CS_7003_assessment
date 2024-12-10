df0c9acbcd9af4a388bac2b523284128a4dc5dbd
Esat Gokcen <esatgokcen173@gmail.com>
2024-12-10 17:22:52 +0000

    created a session, added item data to MenuItems table and commited session

--- src/data_access_layer/menu_management/menu_items.py
+++ src/data_access_layer/menu_management/menu_items.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, Column, Integer, String, Boolean
+from sqlalchemy import create_engine, Column, Integer, String, Boolean, Float
 from sqlalchemy.orm import declarative_base, sessionmaker
 
 engine = create_engine('sqlite:///menu_items.db')
@@ -12,8 +12,21 @@ class MenuItems(Base):
 
     menu_item_id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False)
-    price = Column(Integer, nullable=False)
+    price = Column(Float, nullable=False)
     description = Column(String)
     IsAvailable = Column(Boolean)
 
 Base.metadata.create_all(engine)
+
+# Create session and add data to table
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+item1 = MenuItems(name="Latte", price=3.15, description="single shot milk coffee", IsAvailable=True)
+item2 = MenuItems(name="Flat White", price=3.45, description="double shot milk froth coffee", IsAvailable=True)
+
+session.add_all([item1, item2])
+session.commit()
+
+session.close()
