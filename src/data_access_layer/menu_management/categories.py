8ba26c67f77caffb99be4c729ec8b3f82242fa71
Esat Gokcen <esatgokcen173@gmail.com>
2024-12-11 11:46:29 +0000

    notes from lecture to improve my project

--- src/data_access_layer/menu_management/categories.py
+++ src/data_access_layer/menu_management/categories.py
@@ -1,14 +1,14 @@
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-engine = create_engine('sqlite:///categories.db')
-Base = declarative_base()
+engine = create_engine('sqlite:///categories.db') # KEEP EVERYTHING IN ONE DATABASE
+Base = declarative_base() # IMPORT BASE FOR EACH FILE AND ONLY CALL FUNCTION ONCE
 
 # Create Table
 
 class Categories(Base):
 
-    __tablename__ = "categories"
+    __tablename__ = "categories" # NEED TO USE SQLITE
 
     category_id = Column(Integer, primary_key=True)
     category_name = Column(String, nullable=False)
